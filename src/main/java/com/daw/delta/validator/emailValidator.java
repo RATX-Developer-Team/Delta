24cfdecd86cd4b29b2427dbc1add1adde40d5188
AlexRoojas <100126307+AlexRoojas@users.noreply.github.com>
2022-03-02 21:02:44 +0100

    Email Validator

---
+++ src/main/java/com/daw/delta/validator/emailValidator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daw.delta.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author kete
+ */
+public class emailValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext fc, UIComponent uic, Object o) throws ValidatorException {
+        String email = o.toString();
+
+        if(!email.matches("((?=(.*[a-z]))?)((?=(.*[A-Z]))?)((?=(.*[0-9]))?)[a-zA-Z\\d?]{4,}\\@[a-z]{2,}\\.[a-z]{2,}")){
+            throw new ValidatorException(new FacesMessage("El  email debe cumplir con el formato: minimo 4 caracteres, @, dominio y extension"));
+        } 
+    }
+    
+}
