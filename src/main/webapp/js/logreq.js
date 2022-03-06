47ef1751aea795a60b313acfbc8a864d67ab8201
Alejandro <alexyanamusicpro@gmail.com>
2022-03-06 14:58:09 +0100

    a

--- src/main/webapp/js/logreq.js
+++ src/main/webapp/js/logreq.js
@@ -1,20 +1,20 @@
 $(function() {
-    console.clear();
+    console.clear()
     
     $('.fecha').attr('readonly', false).datepicker({
         dateFormat: 'dd/mm/yy',
         changeMonth: true,
         changeYear: true,
         yearRange: "1920:2021"
-    });
+    })
 
     $('#log').on('click', function() {
-        $('.formuReg').hide();
-        $('.formuLog').show();
-    });
+        $('.formuReg').hide()
+        $('.formuLog').show()
+    })
 
     $('#reg').on('click', function() {
-        $('.formuReg').show();
-        $('.formuLog').hide();
-    });
+        $('.formuReg').show()
+        $('.formuLog').hide()
+    })
 });
